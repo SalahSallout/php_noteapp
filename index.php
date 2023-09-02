f1034ab48a8cb61080b3846bea0769bc79d6a5fa
SalahSallout <s.sallout2002@gmail.com>
2023-09-02 15:44:44 +0300

    Full Database

--- index.php
+++ index.php
@@ -3,10 +3,57 @@
 include "connect.php";
 
 
-$stmt = $con->prepare("SELECT email FROM users WHERE id = 2");
-$stmt->execute();
-$user = $stmt->fetchColumn();
+//جلب جميع البيانات من قاعدة البيانات
 
-$count = $stmt->rowCount();
+// $stmt = $con->prepare("SELECT * FROM users");
+// $stmt->execute();
+// $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $count = $stmt->rowCount();
 
-echo $count;
+// echo "<pre>";
+// print_r($users);
+// echo "</pre>";
+
+// echo json_encode(array("message" => "How Are You"));
+
+
+
+//إضافة على قاعدة البيانات
+
+// $stmt = $con->prepare("INSERT INTO `users`(`username`, `email`) VALUES (?,?)");
+// $stmt->execute(array("Yaser","yaser@gmail.com"));
+
+// $count = $stmt->rowCount();
+
+// if ($count > 0) {
+//     echo "Success";
+// } else {
+//     echo "Falid";
+// }
+
+//تحديث صف في قاعدة البيانات
+
+// $stmt = $con->prepare("UPDATE `users` SET username = ? , email = ?  WHERE id= ?");
+// $stmt->execute(array("Sanad","sanad@gmail.com",1));
+
+// $count = $stmt->rowCount();
+
+// if ($count > 0) {
+//     echo "Success";
+// } else {
+//     echo "Falid";
+// }
+
+
+//حذف صف في قاعدة البيانات
+
+// $stmt = $con->prepare("DELETE FROM users WHERE id= ?");
+// $stmt->execute(array(2));
+
+// $count = $stmt->rowCount();
+
+// if ($count > 0) {
+//     echo "Success";
+// } else {
+//     echo "Falid";
+// }
